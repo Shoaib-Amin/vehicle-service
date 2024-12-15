a45b7daed6e2bd8f39a7bf02966dda7044659712
Shoaib Amin <code007program@gmail.com>
2024-12-16 04:58:23 +0500

    change index file to use serverless

--- api/index.js
+++ api/index.js
@@ -1,48 +1,40 @@
-// const express = require('express');
-// const cors = require('cors')
-// const path = require('path');
-// const serverless = require('serverless-http');
-// require('dotenv').config()
-// const { upload } = require('../middlewares/uploadFiles')
-// const connectDB = require('../configs/db');
-
-// const app = express();
-
-// // Connect to MongoDB
-// connectDB();
-
-
-// const authenticateToken = require('../middlewares/auth');
-
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-// // Serve static files from the 'uploads' directory
-// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-// const authRouter = require('../routes/auth.route')
-// const vehicleRouter = require('../routes/vehicle.route')
-
-// app.use("/auth", authRouter);
-// // Apply the authentication middleware to all routes under /vehicle
-// app.use("/vehicle", authenticateToken, upload.array('images', 10), vehicleRouter);
-
-
-
-// module.exports.handler = serverless(app);
-
-
-
 const express = require('express');
+const cors = require('cors')
+const path = require('path');
+require('dotenv').config()
+const { upload } = require('../middlewares/uploadFiles')
+const connectDB = require('../configs/db');
+
 const app = express();
 
-// Define routes
+// Connect to MongoDB
+connectDB();
+
+
+const authenticateToken = require('../middlewares/auth');
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+// Serve static files from the 'uploads' directory
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+const authRouter = require('../routes/auth.route')
+const vehicleRouter = require('../routes/vehicle.route')
+
+// Test route
 app.get('/', (req, res) => {
-  res.send('Hello from Express app!');
+    res.send('Hello from Express app!');
 });
+
+app.use("/auth", authRouter);
+// Apply the authentication middleware to all routes under /vehicle
+app.use("/vehicle", authenticateToken, upload.array('images', 10), vehicleRouter);
+
+
 
 // Export the app to be used by Vercel
 module.exports = (req, res) => {
-  app(req, res); // Pass the request and response to your Express app
+    app(req, res); // Pass the request and response to your Express app
 };
